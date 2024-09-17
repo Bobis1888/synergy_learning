5a852c54a4b3e4d2d48abcff85db51abcff07b2b
nelmin <bobis1888@gmail.com>
2024-09-17 21:24:07 +0500

    Урок №11. Функции, Правка в первом задании

--- python/lesson_8/part_0.py
+++ python/lesson_8/part_0.py
@@ -5,7 +5,16 @@ def factorial(n):
   else:
     return n * factorial(n - 1)
 
-num = int(input("Введите число: "))
-result = factorial(num)
+def factorial_list(num):
+  result = []
 
-print(result)
+  while num >= 1:
+    result.append(factorial(num))
+    num -= 1
+  return result
+
+number = int(input("Введите число: "))
+
+result_list = factorial_list(number)
+
+print(result_list)
